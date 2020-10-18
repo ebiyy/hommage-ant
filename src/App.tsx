8765ca17aa67f3039ced186abc48681e8ba661e2
ebiyy <kuraim22@gmail.com>
2020-10-18 18:21:28 +0900

    add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from '@material-ui/core';
 import './App.scss';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button color="primary">Hello World</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
